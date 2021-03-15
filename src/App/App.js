8aa221a5ebc819b2c26f5364df0fac34980654da
alex <desorbaix@free.fr>
2021-03-15 15:59:32 +0100

    restructuration de l'app

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>demat breizh</h1>
+      <h2>Bonjour bretagne</h2>
+    </div>
+  );
+}
+
+export default App;
