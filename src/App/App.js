bb5036ce10662fff6470439bf5164d22bf7f9369
alex <desorbaix@free.fr>
2021-03-15 16:53:49 +0100

    button w propTypes and children and props

--- src/App/App.js
+++ src/App/App.js
@@ -1,3 +1,4 @@
+import Button from './components/Button/Button';
 import './App.scss';
 
 function App() {
@@ -5,6 +6,11 @@ function App() {
     <div className="App">
       <h1>demat breizh</h1>
       <h2>Bonjour bretagne</h2>
+      <Button></Button>
+      <Button value="Nouveau"></Button>
+      <Button value="warning" bgColor="tomato"></Button>
+      <Button value="abc" color="tomato"></Button>
+      <Button value="cliquez" bgColor="skyblue" color="darkgrey" ><img src="./logo192.png" width="64px" alt="logo" /></Button>
     </div>
   );
 }
--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,7 +1,18 @@
 import './Button.css';
-function Button()
-{
+import PropTypes from 'prop-types';
+export default function Button(props) {
+    console.log(props);
     return (
-        <div className="button">Benjamin</div>
+        <div className="Button" style={{
+            backgroundColor: undefined !== props.bgColor ? props.bgColor : 'green',
+            color: undefined!==props.color?props.color:'white'
+        }}>
+            {undefined !== props.children && props.children}{props.value}
+        </div>
     );
+}
+Button.propTypes={
+    value:PropTypes.string.isRequired,
+    bgColor:PropTypes.string,
+    color:PropTypes.string
 }
