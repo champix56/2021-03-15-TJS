7e64ff62a50f44843d24587ee851334ea9178175
alex <desorbaix@free.fr>
2021-03-16 12:45:59 +0100

    creation d'un class coponent avec state

--- src/App/components/AppCore/AppCore.js
+++ src/App/components/AppCore/AppCore.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './AppCore.module.scss';
-
-const AppCore = () => (
-  <div className={styles.AppCore} data-testid="AppCore">
-    AppCore Component
-  </div>
-);
-
-AppCore.propTypes = {};
-
-AppCore.defaultProps = {};
-
-export default AppCore;
+import MemeViewer from '../MemeViever/MemeViever';
+import MemeEditor from '../MemeEditor/MemeEditor';
+import style from './AppCore.module.scss';
+export default class AppCore extends React.Component {
+  constructor(props) {
+    //appel du constructeur parent avec les props recus
+    super(props);
+    //initialisation de l'etat du composant
+    this.state = { test: '', titre: 'titre', text: 'text demo', x: 20, y: 20, fontSize: 15, color: '#ACACAC', image: 'img/image1.png' };
+  }
+  render() {
+    return (
+      <>
+        <div>
+          <label>test input :</label>
+          <input type="text" value={this.state.test} onChange={(evt)=>{
+            this.setState({test:evt.target.value})
+          }} />
+        </div>
+        <div className={style.AppCore} test-id="AppCore">
+          <MemeViewer />
+          <MemeEditor />
+        </div>
+        <div>{JSON.stringify(this.state)}</div>
+      </>
+    );
+  }
+}
