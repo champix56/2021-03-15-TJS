d4d0a3bc6e17ba20386e67f1c15f40454d9cf63b
alex <desorbaix@free.fr>
2021-03-16 09:30:01 +0100

    ajout d'un fonction en provenance du parent pour le click

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,18 +1,24 @@
 import './Button.css';
 import PropTypes from 'prop-types';
+/**
+ * Button modulaire couleur et contenu
+ * @param {Object} props bgColor? & color?
+ * @returns component render
+ */
 export default function Button(props) {
     console.log(props);
     return (
         <div className="Button" style={{
             backgroundColor: undefined !== props.bgColor ? props.bgColor : 'green',
             color: undefined!==props.color?props.color:'white'
-        }}>
+        }} onClick={props.onbuttonclicked}>
             {undefined !== props.children && props.children}{props.value}
         </div>
     );
 }
 Button.propTypes={
     value:PropTypes.string.isRequired,
+    onbuttonclicked:PropTypes.func.isRequired,
     bgColor:PropTypes.string,
     color:PropTypes.string
 }
