bc5a81a6a5c8d860bd6062215b334307af80e4e5
alex <desorbaix@free.fr>
2021-03-17 14:37:41 +0100

    transformation de svgDrawer en UI component by props

--- src/App/components/MemeViever/MemeViever.js
+++ src/App/components/MemeViever/MemeViever.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 import styles from './MemeViever.module.scss';
 import SvgDrawer from '../SvgDrawer/SvgDrawer';
+import { emptyMeme, store } from '../../store/store';
 
-const MemeViever = (props) => (
+const MemeViever = (props) => {
+  const [meme, setmeme] = useState(emptyMeme);
+  const [img, setimg] = useState({ w: 0, h: 0 });
+
+  useEffect(() => {
+    store.subscribe(()=>{
+      setmeme(store.getState().currentMeme);
+      const image=store.getState().images.find(e=>String(store.getState().currentMeme.imageId)===String(e.id))
+      //envoie d'une image vide uniquement si 
+      setimg(image?image:{w:0,h:0});
+    })
+  }, []);
+  return(
   <div className={styles.MemeViever} data-testid="MemeViever">
-    <SvgDrawer />
+    <SvgDrawer meme={meme} img={img}/>
   </div>
-);
+);}
 
 export default MemeViever;
